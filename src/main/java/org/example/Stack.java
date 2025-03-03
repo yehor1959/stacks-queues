ab72ec6f78f398ab49f14eba580c66171385653a
yehor1959 <zaitsev.yehor@gmail.com>
2025-03-03 21:02:21 +0100

    Add Constructor to Stack

---
+++ src/main/java/org/example/Stack.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class Stack {
+
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop() {
+        System.out.println("Top: " + top.value);
+    }
+
+    public void getHeight() {
+        System.out.println("Height: " + height);
+    }
+}
