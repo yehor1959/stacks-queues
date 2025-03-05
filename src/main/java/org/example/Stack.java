e7c7fb23e3123e9ba25c2950295b87d70c7d7a25
yehor1959 <zaitsev.yehor@gmail.com>
2025-03-05 11:21:15 +0100

    Add Push method to Stack

--- src/main/java/org/example/Stack.java
+++ src/main/java/org/example/Stack.java
@@ -35,4 +35,13 @@ public class Stack {
     public void getHeight() {
         System.out.println("Height: " + height);
     }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height != 0) {
+            newNode.next = top;
+        }
+        top = newNode;
+        height++;
+    }
 }
