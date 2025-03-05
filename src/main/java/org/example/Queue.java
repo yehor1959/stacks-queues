bd7f9df37a0408bb12b959c5b72611b2e741fa27
yehor1959 <zaitsev.yehor@gmail.com>
2025-03-05 11:55:05 +0100

    Add Constructor for Queue

---
+++ src/main/java/org/example/Queue.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) { this.value = value; }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getFirst() {
+        System.out.println("First: " + first.value);
+    }
+
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
