a27def9355d6a12914e25bcdd1b5dcf1d6eec139
yehor1959 <zaitsev.yehor@gmail.com>
2025-03-05 12:04:30 +0100

    Add Enqueue method to Queue

--- src/main/java/org/example/Queue.java
+++ src/main/java/org/example/Queue.java
@@ -38,4 +38,18 @@ public class Queue {
     public void getLength() {
         System.out.println("Length: " + length);
     }
+
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+
+        if (length == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+
+        length++;
+    }
 }
