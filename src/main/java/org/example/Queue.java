3be6d0994c959af7c070f59fe5e38fc59c9b055a
yehor1959 <zaitsev.yehor@gmail.com>
2025-03-06 04:45:20 +0100

    Add Dequeue method to Queue

--- src/main/java/org/example/Queue.java
+++ src/main/java/org/example/Queue.java
@@ -52,4 +52,20 @@ public class Queue {
 
         length++;
     }
+
+    public Node dequeue() {
+        if (length == 0) return null;
+
+        Node temp = first;
+        if (length == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = temp.next;
+            temp.next = null;
+        }
+
+        length--;
+        return temp;
+    }
 }
